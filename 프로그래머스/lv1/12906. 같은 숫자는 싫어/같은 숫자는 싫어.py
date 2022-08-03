6b3665a94c44cc047758c7b31894302a3253590e
SH Kim <62065749+ksh7621@users.noreply.github.com>
2022-08-03 13:09:15 +0900

    [level 1] Title: 같은 숫자는 싫어, Time: 134.89 ms, Memory: 27.8 MB -BaekjoonHub

---
+++ 프로그래머스/lv1/12906. 같은 숫자는 싫어/같은 숫자는 싫어.py
@@ -0,0 +1,10 @@
+def solution(arr):
+    answer = []
+    for i in range(len(arr)):
+        if(len(answer) == 0):
+            answer.append(arr[i])
+        elif(answer[-1] == arr[i]):
+            continue
+        else:
+            answer.append(arr[i])
+    return answer
